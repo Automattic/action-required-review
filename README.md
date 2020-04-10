b9025bf62c5e41f71ed793549cd79695692477f9
Derek Smart <smart@automattic.com>
2020-04-10 13:04:09 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# action-required-review
-GitHub Action that will check if all review requirements are met for a PR 
+# Required Review 
+
+This GitHub Action will check to see if the required reviewers have accepted the PR. Will fail status checks if not. 
+
+## Usage
+
+This Action subscribes to [Pull request review events](https://developer.github.com/v3/activity/events/types/#pullrequestreviewevent) which fire whenever pull requests are approved. The action requires two environment variables – the label name to add and the number of required approvals. Optionally you can provide a label name to remove.
+
+```workflow
+on: pull_request_review
+name: Check required reviews
+jobs:
+  check_required_reviews:
+    name: Label when approved
+    runs-on: ubuntu-latest
+    steps:
+    - name: Check for required review approval
+      uses: automattic/action-required-review@master
+      env:
+        REQUIRED_REVIEW_TEAM_ID: "12345"
+        GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+```
+
---
+++ entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+  echo "Set the GITHUB_TOKEN env variable."
+  exit 1
+fi
+
+if [[ -z "$GITHUB_REPOSITORY" ]]; then
+  echo "Set the GITHUB_REPOSITORY env variable."
+  exit 1
+fi
+
+if [[ -z "$GITHUB_EVENT_PATH" ]]; then
+  echo "Set the GITHUB_EVENT_PATH env variable."
+  exit 1
+fi
+
+URI="https://api.github.com"
+API_HEADER="Accept: application/vnd.github.v3+json"
+AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
+
+action=$(jq --raw-output .action "$GITHUB_EVENT_PATH")
+state=$(jq --raw-output .review.state "$GITHUB_EVENT_PATH")
+number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
+
+check_for_required_review() {
+  # https://developer.github.com/v3/pulls/reviews/#list-reviews-on-a-pull-request
+  body=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/${GITHUB_REPOSITORY}/pulls/${number}/reviews?per_page=100")
+  reviews=$(echo "$body" | jq --raw-output '.[] | {state: .state} | @base64')
+
+  approvals=0
+
+  for r in $reviews; do
+    review="$(echo "$r" | base64 -d)"
+    rState=$(echo "$review" | jq --raw-output '.state')
+  done
+}
+
+if [[ "$action" == "submitted" ]] && [[ "$state" == "approved" ]]; then
+  check_for_required_review
+else
+  echo "Ignoring event ${action}/${state}"
+fi
